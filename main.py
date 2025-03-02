6c2849fa0c69e3b3163362b957ec11506c6ace40
Aleksei <kybik0606@gmail.com>
2025-03-02 15:03:45 +0300

    Converted the data type of Cabin_num to int. Applied StandardScaler

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder, MinMaxScaler, StandardScaler
-pd.set_option('future.no_silent_downcasting', True)
 
 df = pd.read_csv(r'C:\RAF\4 сем\ИИ и ММО\spaceship-titanic\train.csv')
 print('DATA FROM DATASET:')
@@ -8,6 +7,7 @@ print(df.head(), '\n')
 print(df.info())
 
 df[["Deck", "Cabin_num", "Side"]] = df["Cabin"].str.split("/", expand=True)
+df['Cabin_num'] = df['Cabin_num'].astype(float)
 try:
     df = df.drop('Cabin', axis=1)
     df = df.drop('Name', axis=1)
@@ -15,14 +15,14 @@ except KeyError:
     print("Field does not exist")
 
 missing_values = df.isnull().sum().sort_values(ascending=False)
-print('MISIING VALUES BEFORE FILLING:')
+print('MISSING VALUES BEFORE FILLING:')
 print(missing_values, '\n')
 
 print('DATA FROM DATASET AFTER TRANSFORMATION:')
 print(df.head(), '\n')
 
-categorical_col = ['HomePlanet', 'CryoSleep', 'Deck', 'Cabin_num', 'Side', 'Destination', 'VIP']
-number_col = ['Age', 'RoomService', 'FoodCourt', 'ShoppingMall', 'Spa', 'VRDeck']
+categorical_col = ['HomePlanet', 'CryoSleep', 'Deck', 'Side', 'Destination', 'VIP']
+number_col = ['Cabin_num', 'Age', 'RoomService', 'FoodCourt', 'ShoppingMall', 'Spa', 'VRDeck']
 
 for col in categorical_col:
     df[col] = df[col].fillna(df[col].mode()[0])
@@ -31,7 +31,7 @@ for col in number_col:
     df[col] = df[col].fillna(df[col].mean())
 
 missing_values = df.isnull().sum().sort_values(ascending=False)
-print('MISIING VALUES AFTER FILLING:')
+print('MISSING VALUES AFTER FILLING:')
 print(missing_values, '\n')
 
 categorical_no_bool_col=list(filter(lambda x:x!='CryoSleep'and x!='VIP',categorical_col))
@@ -40,7 +40,7 @@ df['CryoSleep'] = df['CryoSleep'].astype(int)
 df['VIP'] = df['VIP'].astype(int)
 df['Transported'] = df['Transported'].astype(int)
 
-scaler = MinMaxScaler()
+scaler = StandardScaler()
 df[number_col] = scaler.fit_transform(df[number_col])
 df = pd.get_dummies(df, columns=categorical_no_bool_col, prefix=categorical_no_bool_col)
 df = df.replace({True: 1, False: 0})
